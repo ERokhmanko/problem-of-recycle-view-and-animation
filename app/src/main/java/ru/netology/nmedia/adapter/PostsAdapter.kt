d1d4d65f66cd4e884a25ca6eb4b57bd9c125a363
ERokhmanko <rizhii_mishonok@mail.ru>
2022-01-20 13:11:12 +0300

    first commit

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,150 @@
+package ru.netology.nmedia.adapter
+
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.internal.notify
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+
+interface OnInteractionListener {
+    fun onLike(post: Post) {}
+    fun onEdit(post: Post) {}
+    fun onRemove(post: Post) {}
+}
+
+class PostsAdapter(
+    private val onInteractionListener: OnInteractionListener,
+) : ListAdapter<Post, PostViewHolder>(PostDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onInteractionListener)
+    }
+
+    override fun onBindViewHolder(
+        holder: PostViewHolder,
+        position: Int,
+        payloads: List<Any>
+    ) {
+        if (payloads.isEmpty()) {
+            onBindViewHolder(holder, position)
+        } else {
+            payloads.forEach {
+                if (it is Payload) {
+                    holder.bind(it)
+                }
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        (holder.binding.root.tag as? Animator)?.cancel() // проверяем есть ли уже какой-то аниматор и чистим
+        val post = getItem(position)
+        holder.bind(post)
+
+    }
+}
+
+class PostViewHolder(
+    val binding: CardPostBinding,
+    private val onInteractionListener: OnInteractionListener,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(payload: Payload) {
+        payload.liked?.also { liked ->
+            binding.like.setImageResource(
+                if (liked) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+            )
+            if (liked) {
+                // привязываем аниматор к полю
+                val animatorLiked = ObjectAnimator.ofPropertyValuesHolder(
+                    binding.like,
+                    PropertyValuesHolder.ofFloat(View.SCALE_X, 1.0F, 1.2F, 1.0F, 1.2F),
+                    PropertyValuesHolder.ofFloat(View.SCALE_Y, 1.0F, 1.2F, 1.0F, 1.2F)
+                ).apply {
+                    repeatCount = 100
+                    start()
+                }
+                // присваиваем тэгу вью аниматор
+                itemView.tag = animatorLiked
+
+            } else {
+                // привязываем аниматор к полю
+                val animatorNotLiked = ObjectAnimator.ofFloat(
+                    binding.like,
+                    View.ROTATION,
+                    0F, 360F
+                ).apply { start() }
+                //  присваиваем тэгу вью аниматор
+                itemView.tag = animatorNotLiked
+            }
+
+        }
+
+        payload.content?.let(binding.content::setText)
+    }
+
+    fun bind(post: Post) {
+        binding.apply {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            like.setImageResource(
+                if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+            )
+
+            menu.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.options_post)
+                    setOnMenuItemClickListener { item ->
+                        when (item.itemId) {
+                            R.id.remove -> {
+                                onInteractionListener.onRemove(post)
+                                true
+                            }
+                            R.id.edit -> {
+                                onInteractionListener.onEdit(post)
+                                true
+                            }
+
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
+
+            like.setOnClickListener {
+                onInteractionListener.onLike(post)
+            }
+        }
+    }
+}
+
+class PostDiffCallback : DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun getChangePayload(oldItem: Post, newItem: Post): Any =
+        Payload(
+            liked = newItem.likedByMe.takeIf { oldItem.likedByMe != it },
+            content = newItem.content.takeIf { oldItem.content != it },
+        )
+}
+
+data class Payload(
+    val liked: Boolean? = null,
+    val content: String? = null,
+)
